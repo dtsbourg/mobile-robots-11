3454cab2546ab9225df211f6eec7464fdab7537d
dtsbourg <dylan.bourgeois@epfl.ch>
2016-10-27 16:50:25 +0200

    dirdyn

--- userfctR/user_dirdyn.c
+++ userfctR/user_dirdyn.c
@@ -51,8 +51,8 @@ void user_dirdyn_loop(MbsData *mbs_data, MbsDirdyn *mbs_dd)
 
 	// set_plot(mbs_data->q[FJ_T1_robot_B_id], "Real x [m] ");
 	// set_plot(mbs_data->q[FJ_T2_robot_B_id], "Real y pos[m] ");
-	//set_plot(mbs_data->q[FJ_T1_robot_B_id], "Blue x [m] ");
-	//set_plot(mbs_data->q[FJ_R3_robot_B_id], "Blue theta [rad] ");
+	set_plot(mbs_data->q[FJ_T1_robot_B_id], "Blue x [m] ");
+	// set_plot(mbs_data->q[FJ_R3_robot_B_id], "Blue theta [rad] ");
 	simu_loop(mbs_data);
 }
 
