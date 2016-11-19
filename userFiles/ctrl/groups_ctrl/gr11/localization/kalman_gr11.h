2768741bae9d8f59bb0de20a4c8a923423c49d67
dtsbourg <dylan.bourgeois@epfl.ch>
2016-11-19 13:47:59 +0100

    init KF

--- userFiles/ctrl/groups_ctrl/gr11/localization/kalman_gr11.h
+++ userFiles/ctrl/groups_ctrl/gr11/localization/kalman_gr11.h
@@ -1,4 +1,4 @@
-/*! 
+/*!
  * \author Group 11
  * \file kalman_gr11.h
  * \brief localization sensors fusion with Kalman
@@ -12,10 +12,40 @@
 
 NAMESPACE_INIT(ctrlGr11);
 
+typedef struct {
+	double ** matrix;
+	int rows;
+	int cols;
+} matrix_t;
+
 /// Kalman main structure
 struct KalmanStruc
 {
-	int dummy_variable; ///< put your own variable, this is just an example without purpose
+	/// State vectors
+	double * x; //< State
+	double * u; //< State Input
+	double * y; //< State Observed
+	double * z; //< Innovation
+
+	/// Covariances
+	double * P; //< Covariance of prediction error
+    double * Q; //< Covariance of process noise
+	double * E; //< Covariance of expectation
+    double * R; //< Covariance of measurement noise
+	double * Z; //< Covariance of innovation
+
+
+	/// Jacobians
+	double * H;		//< Jacobian of measurement model
+	double * F_x;	//< Jacobian of state model
+	double * F_u;	//< Jacobian of input model
+	double * F_n;	//< Jacobian of noise model
+
+	/// Kalman
+	double * K; //< Kalman gain
+
+	/// Utility
+	double * tmp;
 };
 
 void kalman(CtrlStruct *cvs);
