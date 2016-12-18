bb181fe4abafc25de9a62049a20fb92d149e08a9
dtsbourg <dtsbourg@gmail.com>
2016-12-18 14:45:38 +0100

    refactor path_planning

--- userFiles/ctrl/groups_ctrl/gr11/path/path_planning_gr11.h
+++ userFiles/ctrl/groups_ctrl/gr11/path/path_planning_gr11.h
@@ -12,44 +12,67 @@
 
 NAMESPACE_INIT(ctrlGr11);
 
-/// cell struct to store a cell
+#define MAP_SIZE_X 17
+#define MAP_SIZE_Y 27
+
+/// Cell struct
+/*
+ * This struct wraps a map's cell
+ */
 struct Cell
 {
-	int x;	// x coordinate
-	int y;	// y coordinate
+	int x;	///< x coordinate
+	int y;	///< y coordinate
 };
-/// store the path
+
+/// Path struct
+/*
+ * A Path throught the map is a linked list
+ * of Cells.
+ */
 struct Path
 {
 	Cell cell;
 	Path* next;
 };
 
+/// Node struct
+/*
+ * The path planning algorithm builds a linked list
+ * of Nodes.
+ */
 struct Node{
-	Node* parent;
-	Cell cell;
-	float f,g,h;
-	Node* next;
+	Node* parent;   // The parent node in the graph
+	Node* next;     // The next node in the graph
+	Cell cell;      // The corresponding cell
+	float f, g, h;  // The scores for the A* algorithm
 };
 
-// main algo function
-void* path_planning(Cell start, Cell goal, bool map[17][27], Path* old_path, bool return_distance);
-// Evaluate distance between 2 cells on the map
+/// Main path planning
+void* path_planning(Cell start, Cell goal, bool map[MAP_SIZE_X][MAP_SIZE_Y], Path* old_path, bool return_distance);
+
+/// Returns the distance between 2 cells on the map
 float evaluate_distance(Cell cell1, Cell cell2);
-// Return the 8 cells arround a cell
-void get_cells_arround(Cell* cell_arround, Cell cell);
-// Generate path from the goal_node
+
+/// Return the 8 cells neighborhood arround a cell
+void get_cell_neighbourhood(Cell* cell_arround, Cell cell);
+
+/// Generate path from the goal_node
 Path* generate_path(Node* goal_node);
-// check if a cell is viable to go in the list
+
+/// Check if a cell is viable to go in the list
 bool cell_is_viable(Cell cell, bool map[17][27]);
-// check if the node is already in a list
+
+/// Check if the node is already in the list
 bool is_in_list(Node* list, Node* element);
-// free nodes list:
+
+/// free nodes list
 void free_nodes(Node* list);
-// free path list:
+
+/// free path list
 void free_path(Path* path);
 
-// test function to display list of nodes:
+/// Debug function to display list of nodes
 void display_nodes(Node* current);
 
 
